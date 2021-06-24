730f717ecd497d2486eee53d71ca3d028b7b98f4
narayana.swamy <nswamy751@gmail.com>
2021-06-25 01:04:02 +0530

    initial commit

---
+++ src/main/java/com/ns/java8streams/java8streams/Java8StreamsApplication.java
@@ -0,0 +1,13 @@
+package com.ns.java8streams.java8streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8StreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8StreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ns/java8streams/java8streams/Java8StreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns.java8streams.java8streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8StreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
